9f4a5f979d1ac17d6ab8d731b6dce2b9a7b4d422
jaideepghosh <er.jaideepghosh@gmail.com>
2022-07-02 14:04:49 +0530

    feat:Added material-ui

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -13,6 +13,10 @@
         <!-- Styles -->
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"/>
+
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"/>
+
         <!-- Scripts -->
         @routes
         <script src="{{ mix('js/app.js') }}" defer></script>
